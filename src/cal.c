2899e152ac862d54d8575150a03c26cac3985608
Pekka Paalanen <pq@iki.fi>
2016-07-19 16:22:33 +0300

    cal: move stuff from cal.c to cal.h
    The display and window structs will be needed in renderer and input .c
    files.
    
    Signed-off-by: Pekka Paalanen <pq@iki.fi>

--- src/cal.c
+++ src/cal.c
@@ -46,6 +46,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "cal.h"
 #include "platform.h"
 #include "helpers.h"
 #include "xalloc.h"
@@ -56,83 +57,6 @@
 #include "presentation-time-client-protocol.h"
 
 #define TITLE PACKAGE_STRING " cal"
-
-#define INVALID_CLOCK_ID 9999
-
-struct window;
-struct seat;
-
-struct submission {
-	struct wl_list link;
-	struct window *window;
-
-	struct wl_callback *frame;
-	bool frame_done;
-	struct wp_presentation_feedback *feedback;
-
-	struct output *sync_output;
-};
-
-struct display {
-	struct wl_display *display;
-	struct wl_registry *registry;
-	struct wl_compositor *compositor;
-	struct wl_shell *shell;
-
-	struct wp_presentation *presentation;
-	clockid_t clock_id;
-	struct oring_clock gfx_clock;
-
-	struct wl_seat *seat;
-	struct wl_pointer *pointer;
-	struct wl_keyboard *keyboard;
-
-	struct wl_shm *shm;
-	struct wl_cursor_theme *cursor_theme;
-	struct wl_cursor *default_cursor;
-	struct wl_surface *cursor_surface;
-
-	struct {
-		EGLDisplay dpy;
-		EGLContext ctx;
-		EGLConfig conf;
-	} egl;
-	struct window *window;
-
-	struct wl_list output_list; /* struct output::link */
-};
-
-struct geometry {
-	int width, height;
-};
-
-struct window {
-	struct display *display;
-	struct geometry geometry, window_size;
-	struct {
-		GLuint rotation_uniform;
-		GLuint pos;
-		GLuint col;
-	} gl;
-
-	uint32_t benchmark_time, frames;
-	struct wl_egl_window *native;
-	struct wl_surface *surface;
-	struct wl_shell_surface *shsurf;
-	EGLSurface egl_surface;
-	struct wl_callback *callback;
-
-	bool fullscreen;
-	bool opaque;
-
-	struct wl_list submissions_list; /* struct submission::link */
-	struct wl_list on_output_list; /* struct window_output::link */
-};
-
-struct window_output {
-	struct output *output;
-	struct wl_list link;
-};
 
 static const char *vert_shader_text =
 	"uniform mat4 rotation;\n"
---
+++ src/cal.h
@@ -0,0 +1,119 @@
+/*
+ * Copyright © 2011 Benjamin Franzke
+ * Copyright © 2016 Pekka Paalanen <pq@iki.fi>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef ORING_CAL_H
+#define ORING_CAL_H
+
+#include <stdbool.h>
+#include <time.h>
+
+#include <wayland-client.h>
+
+#include <GLES2/gl2.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#include "oring-clock.h"
+#include "output.h"
+
+#include "presentation-time-client-protocol.h"
+
+#define INVALID_CLOCK_ID 9999
+
+struct window;
+struct seat;
+
+struct submission {
+	struct wl_list link;
+	struct window *window;
+
+	struct wl_callback *frame;
+	bool frame_done;
+	struct wp_presentation_feedback *feedback;
+
+	struct output *sync_output;
+};
+
+struct display {
+	struct wl_display *display;
+	struct wl_registry *registry;
+	struct wl_compositor *compositor;
+	struct wl_shell *shell;
+
+	struct wp_presentation *presentation;
+	clockid_t clock_id;
+	struct oring_clock gfx_clock;
+
+	struct wl_seat *seat;
+	struct wl_pointer *pointer;
+	struct wl_keyboard *keyboard;
+
+	struct wl_shm *shm;
+	struct wl_cursor_theme *cursor_theme;
+	struct wl_cursor *default_cursor;
+	struct wl_surface *cursor_surface;
+
+	struct {
+		EGLDisplay dpy;
+		EGLContext ctx;
+		EGLConfig conf;
+	} egl;
+	struct window *window;
+
+	struct wl_list output_list; /* struct output::link */
+};
+
+struct geometry {
+	int width, height;
+};
+
+struct window {
+	struct display *display;
+	struct geometry geometry, window_size;
+	struct {
+		GLuint rotation_uniform;
+		GLuint pos;
+		GLuint col;
+	} gl;
+
+	uint32_t benchmark_time, frames;
+	struct wl_egl_window *native;
+	struct wl_surface *surface;
+	struct wl_shell_surface *shsurf;
+	EGLSurface egl_surface;
+	struct wl_callback *callback;
+
+	bool fullscreen;
+	bool opaque;
+
+	struct wl_list submissions_list; /* struct submission::link */
+	struct wl_list on_output_list; /* struct window_output::link */
+};
+
+struct window_output {
+	struct output *output;
+	struct wl_list link;
+};
+
+#endif /* ORING_CAL_H */
